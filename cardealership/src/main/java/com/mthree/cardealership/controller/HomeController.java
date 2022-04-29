95206737021d78e5e1a18210f78ea2b9b694ea26
Juan <jcbritov2@gmail.com>
2022-04-29 15:50:42 -0400

    displaying cars

--- cardealership/src/main/java/com/mthree/cardealership/controller/HomeController.java
+++ cardealership/src/main/java/com/mthree/cardealership/controller/HomeController.java
@@ -4,6 +4,10 @@
  */
 package com.mthree.cardealership.controller;
 
+import com.mthree.cardealership.dao.CarDao;
+import com.mthree.cardealership.entities.Car;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +20,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-//    @Autowired
-//    CarDao dao;
-    
+    @Autowired
+    CarDao dao;
+
     @RequestMapping("home")
-    public String index() {
+    public String index(Model model) {
+
+        List<Car> cars = dao.getAllCars();
+        model.addAttribute("/", cars);
+
         return "/";
     }
 
