23cff9c9ba0d477d8fecf1cce38cb11c43f8076a
Juan <jcbritov2@gmail.com>
2022-04-29 14:03:37 -0400

    home page and new cars page

--- cardealership/src/main/java/com/mthree/cardealership/controller/HomeController.java
+++ cardealership/src/main/java/com/mthree/cardealership/controller/HomeController.java
@@ -4,12 +4,10 @@
  */
 package com.mthree.cardealership.controller;
 
-import com.mthree.cardealership.dao.CarDao;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
@@ -20,11 +18,15 @@ public class HomeController {
 
 //    @Autowired
 //    CarDao dao;
+    
+    @RequestMapping("home")
+    public String index() {
+        return "/";
+    }
 
     @GetMapping("new")
     public String displayTeachers(Model model) {
-        
-        
+
         return "new";
     }
 
